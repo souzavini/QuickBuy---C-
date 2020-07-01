ce0058e278f33c38d6c3553e2fd4325562e57e3c
Vinicius Souza <faw.viniciussouza@gmail.com>
2020-06-30 23:25:06 -0300

    Finalização das entidades,repositorios e contratos.

---
+++ QuickBuy.Dominio/Entidades/Pedido.cs
@@ -0,0 +1,44 @@
+﻿using QuickBuy.Dominio.ObjetoDeValor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace QuickBuy.Dominio.Entidades
+{
+    public class Pedido : Entidade
+    {
+        public int Id { get; set; }
+
+        public DateTime DataPedido { get; set; }
+
+        public int UsuarioId { get; set; }
+
+        public DateTime DataPrevisaoEntrega { get; set; }
+
+        public string CEP { get; set; }
+
+        public string Estado { get; set; }
+
+        public string  Cidade { get; set; }
+
+        public string EnderecoCompleto { get; set; }
+
+        public int NumeroEndereco { get; set; }
+
+        public int FormaPagamentoId { get; set; }
+
+        public FormaPagamento FormaPagamento { get; set; }
+
+        public ICollection<ItemPedido> ItensPedido { get; set; }
+
+        public override void Validate()
+        {
+            LimparMensagensValidacao();
+            if (!ItensPedido.Any())
+                AdicionarCritica("Critica - Pedido não pode ficar sem item de pedido");
+            if (string.IsNullOrEmpty(CEP))
+                AdicionarCritica("Crítica - CEP deve estar preenchido");
+        }
+    }
+}
