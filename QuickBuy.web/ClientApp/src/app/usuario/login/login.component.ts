a5a50003ae74260d8190492cef911f09e617631e
Vinicius Souza <faw.viniciussouza@gmail.com>
2020-07-11 01:28:31 -0300

    Finalizando Angular template com rotas e validaçao de usuario

--- QuickBuy.web/ClientApp/src/app/usuario/login/login.component.ts
+++ QuickBuy.web/ClientApp/src/app/usuario/login/login.component.ts
@@ -1,10 +1,35 @@
-import { Component } from "@angular/core"
+import { Component, OnInit } from "@angular/core";
+import { Usuario } from "src/app/modelo/usuario";
+import { Router, ActivatedRoute} from "@angular/router"
+
 
 @Component({
     selector: "app-login",
     templateUrl: "./login.component.html",
     styleUrls: [ "./login.component.css" ]
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+    public returnUrl: string;
+    public usuario;
+
+    constructor(private router: Router, private activatedRouter:ActivatedRoute){
+        
+    }
+
+    ngOnInit(): void {
+        this.usuario = new Usuario();
+        this.returnUrl = this.activatedRouter.snapshot.queryParams['returnUrl'];
+    }
+
+    
+   
+   
+    entrar(): void{
+        alert(this.returnUrl)
+        if(this.usuario.email =="leo@teste.com" && this.usuario.senha =="abc123"){
+           sessionStorage.setItem("usuario-autenticado","1");
+           this.router.navigate([this.returnUrl]);
+        }
+    }
     
 }
